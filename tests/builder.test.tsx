7bdb9f643ef692d9ec4fb2364b21963b7c74c21f
yw662 <yw662@njit.edu>
2021-04-03 01:08:50 -0700

    fix a testing bug with unsorted array equality assertion

--- tests/builder.test.tsx
+++ tests/builder.test.tsx
@@ -68,7 +68,7 @@ Deno.test('build', async () => {
       { name: 'index.js', isFile: true, isDirectory: false, isSymlink: false },
       { name: 'sth-else', isFile: true, isDirectory: false, isSymlink: false }
     ],
-    Array.from(Deno.readDirSync('./tests/builder/dist'))
+    Array.from(Deno.readDirSync('./tests/builder/dist')).sort((a, b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0)
   )
   assertEquals(
     {
